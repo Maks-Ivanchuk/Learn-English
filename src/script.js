af0bdc42c8dc06102f4dc00725d2348e0d559dc3
Maks-Ivanchuk <maksivanchu@gmail.com>
2023-03-21 11:06:59 +0200

    add

---
+++ src/script.js
@@ -0,0 +1,7 @@
+const bodyElement = document.body;
+
+const firstChildNode = bodyElement.firstChild;
+const lastChildNode = bodyElement.lastChild;
+
+console.log(firstChildNode);
+console.log(lastChildNode);
