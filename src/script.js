a0034ee84b5465bf28600189c6f04b292679d07e
Maks-Ivanchuk <maksivanchu@gmail.com>
2023-03-22 14:05:52 +0200

    add burger button

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
 function stopBurgerMenu() {
-   if (document.documentElement.clientWidth > 764) {
+   if (document.documentElement.clientWidth > 768) {
       document.querySelector('.header__nav').classList.remove('header__nav--open-burger');
    }
 }
 
-document.querySelector('.header__burger-menu').addEventListener("click", function() {
+document.querySelector('.header__button').addEventListener("click", function() {
    document.querySelector('.header__nav').classList.toggle('header__nav--open-burger')
    stopBurgerMenu();
 });
