8f9af0320b212b910693f2e47b29dc6eff757424
Maks-Ivanchuk <maksivanchu@gmail.com>
2023-03-27 13:38:38 +0300

    finished burger menu

--- src/script.js
+++ src/script.js
@@ -1,11 +1,21 @@
-function stopBurgerMenu() {
-   if (document.documentElement.clientWidth > 768) {
-      document.querySelector('.header__nav').classList.remove('header__nav--open-burger');
-   }
+// function stopBurgerMenu() {
+//    if (document.documentElement.clientWidth > 768) {
+//       document.querySelector('.header__nav').classList.remove('header__nav--open-burger');
+//    }
+// }
+
+// function stopBurgerMenu() {
+//    if (window.innerWidth > 768) {
+//       document.querySelector('.header__nav').classList.remove('header__nav--open-burger');
+//    }
+// }
+function burgerMenu() {
+   document.querySelector('.header__button').addEventListener("click", function () {
+      document.querySelector('.header__nav').classList.toggle('header__nav--open-burger');
+
+      document.querySelector('.header__burger-menu').classList.toggle('header__burger-menu--activate-burger-button')
+
+      document.querySelector('.header__button span').classList.toggle('.span-active')
+   });
 }
-
-document.querySelector('.header__button').addEventListener("click", function() {
-   document.querySelector('.header__nav').classList.toggle('header__nav--open-burger')
-   stopBurgerMenu();
-});
-
+burgerMenu()
