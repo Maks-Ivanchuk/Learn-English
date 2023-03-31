9c58f68d2f8a45cfabda460fea9c8b4428343fa6
Maks-Ivanchuk <maksivanchu@gmail.com>
2023-03-31 16:38:43 +0300

    corected burger menu

--- src/script.js
+++ src/script.js
@@ -1,20 +1,28 @@
-// function stopBurgerMenu() {
-//    if (document.documentElement.clientWidth > 768) {
-//       document.querySelector('.header__nav').classList.remove('header__nav--open-burger');
-//    }
-// }
 
-// function stopBurgerMenu() {
-//    if (window.innerWidth > 768) {
-//       document.querySelector('.header__nav').classList.remove('header__nav--open-burger');
-//    }
-// }
+/* ------------- var 1 -------------
+
 function burgerMenu() {
-   document.querySelector('.header__button').addEventListener("click", function () {
-      document.querySelector('.header__nav').classList.toggle('header__nav--open-burger');
+   document.querySelector('.header__burger-button').addEventListener("click", function () {
+      document.querySelector('.header__nav').classList.toggle('header__nav--open-burger-menu');
 
       document.querySelector('.header__burger-menu').classList.toggle('header__burger-menu--activate-burger-button')
    });
 }
 
-burgerMenu()
+burgerMenu();
+---------------------------------- */
+
+/* ------------- var 2 -------------*/
+let bugregButton = document.querySelector('.header__burger-button');
+let bugregMenu = document.querySelector('.header__burger-menu');
+let headerNavigation = document.querySelector('.header__nav');
+
+document.addEventListener("DOMContentLoaded", burgerMenu());
+
+function burgerMenu() {
+   bugregButton.addEventListener("click", function () {
+      headerNavigation.classList.toggle('header__nav--open-burger-menu');
+
+      bugregMenu.classList.toggle('header__burger-menu--activate-burger-button');
+   });
+}
