0ef364f39ceee2be39f0d47aee813a665535d5ef
Maks-Ivanchuk <maksivanchu@gmail.com>
2023-04-03 21:18:40 +0300

    corected

--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
-const body = document.body;
-const burgerButton = document.querySelector('.header__burger-menu');
-const wrapperBurgerButton = document.querySelector('.header__wrapper-burger-menu');
-const headerNavigation = document.querySelector('.header__nav');
 
+document.addEventListener("DOMContentLoaded", () => {
 /*----------- var 3 ------ хуй зна шо роблю-------*/
+   const body = document.body;
+   const burgerButton = document.querySelector('.header__burger-menu');
+   const wrapperBurgerButton = document.querySelector('.header__wrapper-burger-menu');
+   const headerNavigation = document.querySelector('.header__nav');
 
-function openBurgerMenu() {
    burgerButton.addEventListener('click', function () {
       headerNavigation.classList.toggle('header__nav--open-burger-menu');
       body.classList.toggle('lock');
@@ -15,8 +15,7 @@ function openBurgerMenu() {
       event.target.parentElement.classList.remove('header__nav--open-burger-menu');
       body.classList.remove('lock');
    });
-};
+});
 
-document.addEventListener("DOMContentLoaded", openBurgerMenu());
 
 /*-----------end var 3 ------ хуй зна шо наробив-------*/
