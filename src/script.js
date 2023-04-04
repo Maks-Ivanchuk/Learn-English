0d31d809f3e497ca686c89b158f1cf00323cfcd8
Maks-Ivanchuk <maksivanchu@gmail.com>
2023-04-04 14:25:52 +0300

    corected js code burger

--- src/script.js
+++ src/script.js
@@ -1,21 +1,18 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-/*----------- var 3 ------ хуй зна шо роблю-------*/
+
    const body = document.body;
    const burgerButton = document.querySelector('.header__burger-menu');
-   const wrapperBurgerButton = document.querySelector('.header__wrapper-burger-menu');
    const headerNavigation = document.querySelector('.header__nav');
 
-   burgerButton.addEventListener('click', function () {
-      headerNavigation.classList.toggle('header__nav--open-burger-menu');
+   burgerButton.addEventListener('click', function (event) {
+      //headerNavigation.classList.toggle('header__nav--open-burger-menu');
+      event.target.closest('.container').querySelector('.header__nav').classList.toggle('header__nav--open-burger-menu');
       body.classList.toggle('lock');
    });
-   
+
    headerNavigation.addEventListener('click', function (event) {
       event.target.parentElement.classList.remove('header__nav--open-burger-menu');
       body.classList.remove('lock');
    });
 });
-
-
-/*-----------end var 3 ------ хуй зна шо наробив-------*/
