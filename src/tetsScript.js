7e585a5f7bb5912984522c0d9dfb69e974bc3653
Maks Ivanchuk <maksivanchu@gmail.com>
2023-04-07 15:40:56 +0300

    asdasd

--- src/tetsScript.js
+++ src/tetsScript.js
@@ -2,7 +2,6 @@
 
 document.addEventListener("DOMContentLoaded", () => {
    const tree = document.querySelector('#tree');
-   let targetForBtnClose = document.querySelector('.targetFotBtnClose');
 
    function addSpan() {
       for (let li of tree.querySelectorAll('li')) {
@@ -18,31 +17,65 @@ document.addEventListener("DOMContentLoaded", () => {
    tree.addEventListener('click', (event) => {
       let target = event.target;
       let childContainer = target.parentNode.querySelector('ul');
-      let sd = childContainer.children.querySelector('ul');
+      let closeAll = target.closest('.targetForBtnClose').querySelector('ul');
       /*---------------------------------------------------------*/
       function dropDownMenuList() {
          if (target.tagName == 'SPAN' && childContainer) {
             childContainer.hidden = !childContainer.hidden;
-         }
-      }
-      /*---------------------------------------------------------*/
-      function hiddenStepButton() {
-         if (target.tagName == 'BUTTON' && childContainer) {
-            if (childContainer.hidden) {
-               childContainer.hidden = !childContainer.hidden;
-            };
-         }
-      };
-      /*---------------------------------------------------------*/
-      function closeButton() {
-         if (target.tagName == 'BUTTON' && !childContainer) {
-            if (!closeAll) return;
-            closeAll.hidden = !closeAll.hiden;
          };
       };
       /*---------------------------------------------------------*/
-      dropDownMenuList();
-      hiddenStepButton();
-      closeButton();
-   });
+      function firstButtonStepHidden() {
+         if (target.tagName == 'BUTTON' && childContainer) {//шукаємо батю а в ньому Ул
+            if (childContainer.hidden) {
+               childContainer.hidden = !childContainer.hidden;
+               console.log(childContainer);
+            } else if (!childContainer.hidden) {//другий рівень вкладеності
+               let nextchildContainer = childContainer.children; //дізнаємось скільки дітей в ul
+               let numChildren = nextchildContainer.length;
+
+               for (let i = 0; i < numChildren; i++) {  //перебрали всі   ul  li
+                  let ulInChildren = nextchildContainer[i].querySelector('ul');
+
+                  if (ulInChildren.hidden) {
+                     ulInChildren.hidden = !ulInChildren.hidden;
+                  } else {
+                     return;
+                  }
+                  console.log(ulInChildren);
+               }
+            };
+         };
+      };
+      /*---------------------------------------------------------*/
+      function sortChildrenContainer() {
+         let nextchildContainer = childContainer.children; //дізнаємось скільки дітей в ul
+         let numChildren = nextchildContainer.length;
+
+         if (childContainer.hidden) {//другий рівень вкладеності
+            childContainer.hidden = !childContainer.hidden;
+            if ()
+               for (let i = 0; i < numChildren; i++) {  //перебрали всі   ul  li
+                  let ulInChildren = nextchildContainer[i].querySelector('ul');
+
+                  if (ulInChildren.hidden) {
+                     ulInChildren.hidden = !ulInChildren.hidden;
+                  } else {
+                     return;
+                  }
+                  console.log(ulInChildren);
+               }
+         };
+         /*---------------------------------------------------------*/
+         function endButtonHiddenAll() {
+            if (target.tagName == 'BUTTON' && !childContainer) {
+               if (!closeAll) return;
+               closeAll.hidden = !closeAll.hiden;
+            };
+         };
+         /*---------------------------------------------------------*/
+         dropDownMenuList();
+         firstButtonStepHidden();
+         endButtonHiddenAll();
+      });
 });
